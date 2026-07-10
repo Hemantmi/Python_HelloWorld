43d0d98a371bdda1bece91eaaf4cd64de0e00147
Hemantmi <hemantmishra1988@gmail.com>
2026-07-10 11:43:04 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        if (len(s)!=len(t)):
+            return False
+
+        created_hash ={}
+        created_hash2 ={}
+
+        for char in s:
+            if char not in created_hash:
+                created_hash[char]=1
+            else:
+                created_hash[char]+=1
+
+        for char in t:
+            if char not in created_hash2:
+                created_hash2[char]=1
+            else:
+                created_hash2[char]+=1
+
+        if created_hash==created_hash2:
+            return True
+        else:
+            return False
